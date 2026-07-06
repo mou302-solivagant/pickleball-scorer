7a12b7b9a00a07ab273852e32f2713526a1e81d1
mou302 <mou302@gmail.com>
2026-07-06 22:47:54 +0800

    調整森林綠主題配色,加強與預設主題的區隔度

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pickleball-scorer-v5';
+const CACHE = 'pickleball-scorer-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
